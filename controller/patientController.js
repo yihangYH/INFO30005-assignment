d0e478d9005a13aa7523a28c2aea8f5f40b31367
liuka <liuka@student.unimelb.edu.au>
2022-04-23 20:27:21 +0800

    only allow patient to update data once per day

--- controller/patientController.js
+++ controller/patientController.js
@@ -36,12 +36,7 @@ const updateData  = async(req,res,next) =>{
             comment:req.body.blood_glucose_comment,
         })
 
-        if(leastTime.split('/')[1] == currentMonth && leastTime.split('/')[0] == currentDay && req.body.blood_glucose.replace(/\s/g,'')!= leastValue){
-            // await bloodGlucose.findOneAndUpdate(
-            //     {_id:patient.bloodGlucose[patient.bloodGlucose.length - 1]._id}, 
-            //     {value: req.body.blood_glucose, time:dateString}
-            // )
-        }else if(leastTime.split('/')[1] != currentMonth || leastTime.split('/')[0] != currentDay){
+        if(leastTime.split('/')[1] != currentMonth || leastTime.split('/')[0] != currentDay){
             await Patient.findOneAndUpdate({_id:req.params.id}, {$push: {bloodGlucose: data._id}});
             data.save() 
         }
@@ -54,12 +49,7 @@ const updateData  = async(req,res,next) =>{
             time:dateString,
             comment:req.body.weight_comment,
         })
-        if(leastTime.split('/')[1] == currentMonth && leastTime.split('/')[0] == currentDay && req.body.weight.replace(/\s/g,'')!= leastValue){
-            // await weight.findOneAndUpdate(
-            //     {_id:patient.weight[patient.weight.length - 1]._id}, 
-            //     {value: req.body.weight, time:dateString}
-            // )
-        }else if(leastTime.split('/')[1] != currentMonth || leastTime.split('/')[0] != currentDay){
+        if(leastTime.split('/')[1] != currentMonth || leastTime.split('/')[0] != currentDay){
             await Patient.findOneAndUpdate({_id:req.params.id}, {$push: {weight: data._id}});
             data.save() 
         }
@@ -72,12 +62,7 @@ const updateData  = async(req,res,next) =>{
             time:dateString,
             comment:req.body.insulin_comment,
         })
-        if(leastTime.split('/')[1] == currentMonth && leastTime.split('/')[0] == currentDay && req.body.insulin_taken.replace(/\s/g,'')!= leastValue){
-            // await insulinTaken.findOneAndUpdate(
-            //     {_id:patient.insulinTaken[patient.insulinTaken.length - 1]._id}, 
-            //     {value: req.body.insulin_taken, time:dateString}
-            // )
-        }else if(leastTime.split('/')[1] != currentMonth || leastTime.split('/')[0] != currentDay){
+        if(leastTime.split('/')[1] != currentMonth || leastTime.split('/')[0] != currentDay){
             await Patient.findOneAndUpdate({_id:req.params.id}, {$push: {insulinTaken: data._id}});
             data.save()
         }
@@ -90,12 +75,7 @@ const updateData  = async(req,res,next) =>{
             time:dateString,
             comment:req.body.exercise_comment,
         })
-        if(leastTime.split('/')[1] == currentMonth && leastTime.split('/')[0] == currentDay && req.body.exercise!= leastValue){
-            // await exercise.findOneAndUpdate(
-            //     {_id:patient.exercise[patient.exercise.length - 1]._id}, 
-            //     {value: req.body.exercise, time:dateString}
-            // )
-        }else if(leastTime.split('/')[1] != currentMonth || leastTime.split('/')[0] != currentDay){
+        if(leastTime.split('/')[1] != currentMonth || leastTime.split('/')[0] != currentDay){
             await Patient.findOneAndUpdate({_id:req.params.id}, {$push: {exercise: data._id}});
             data.save() 
         }
