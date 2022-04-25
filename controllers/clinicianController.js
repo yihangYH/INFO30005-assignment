5d8db9a4198988d6931af585b6ec373a0d625286
Lihui Hu <lihui@student.unimelb.edu.au>
2022-04-25 15:44:36 +1000

    Update clinicianController.js

--- controllers/clinicianController.js
+++ controllers/clinicianController.js
@@ -8,9 +8,12 @@ const getClinician = async (req,res, next) =>{
             await clinicianData.findOne({first_name: "Michael"}).populate("patient").lean()
         
         for(let i = 0; i < Object.keys(clinician.patient).length; i++){
-            const patientData = await patient.findById({_id:clinician.patient[i]._id}).populate('weight')
-            // console.log(patientData)
+            const patientData = await patient.findById({_id:clinician.patient[i]._id}).populate("weight").
+            populate("exercise").populate("bloodGlucose").populate("insulinTaken").lean()
             clinician.patient[i].weight = patientData.weight
+            clinician.patient[i].bloodGlucose = patientData.bloodGlucose
+            clinician.patient[i].insulinTaken = patientData.insulinTaken
+            clinician.patient[i].exercise = patientData.exercise
         }
         console.log(clinician.patient[0])
         return res.render('allPatient.hbs', {data: clinician})
