b1d78d84dd4c52c79f25b82eee08cfb627d5c134
Laohan <hlaohan666@gmail.com>
2022-04-25 19:15:23 +0800

    fix css of time

--- helper/hbsHelper.js
+++ helper/hbsHelper.js
@@ -9,6 +9,20 @@ hbs.handlebars.registerHelper("getValue", function(data) {
 
 hbs.handlebars.registerHelper("getTime", function(data) {
     const latest = data[data.length-1].time
-    const time = latest.split()[1]
-    return time
+    if (latest){
+        const time = latest.split(" ")[2].split(":")
+        return twoDigit(time[0])+":"+twoDigit(time[1])+" "+latest.split(" ")[3]
+    }
+    else{
+        return latest
+    }
+    
 });
+
+function twoDigit(data){
+    if (data.length<2){
+        return "0"+data
+    }else{
+        return data
+    }
+}
