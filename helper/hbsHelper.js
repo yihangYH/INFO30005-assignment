8e921971bfb72c6815c4682121d39a80cb0f98b1
Yihang Liu <yihang1@student.unimelb.edu.au>
2022-04-25 18:37:08 +0800

    implement check safety threshold

--- helper/hbsHelper.js
+++ helper/hbsHelper.js
@@ -12,3 +12,16 @@ hbs.handlebars.registerHelper("getTime", function(data) {
     const time = latest.split()[1]
     return time
 });
+
+hbs.handlebars.registerHelper("checkSafety", function(safety,value,index) {
+    const bound =  safety[index];
+    if(!bound.includes("Not Required")){
+        const lower = Number(bound.split("-")[0])
+        const upper = Number(bound.split("-")[1])
+        const latestValue = value[value.length-1].value
+        if(latestValue<=lower || latestValue>=upper){
+            return "color: red; font-style: italic; font-weight: bold; "
+        }
+    }
+    return ""
+});
