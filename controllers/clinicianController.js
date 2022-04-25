6ed53bcd892cb045079d70a9062d49870052ef04
Yihang Liu <yihang1@student.unimelb.edu.au>
2022-04-25 13:29:26 +0800

    update

--- controllers/clinicianController.js
+++ controllers/clinicianController.js
@@ -1,12 +1,18 @@
 const clinicianData = require("../models/clinician")
 const {patient} = require("../models/patient")
+require('../models/data')
 const getClinician = async (req,res, next) =>{
     // res.render('allPatient.hbs', {data: clinicianData})
     try{
         const clinician = 
-            await clinicianData.findOne({first_name: "Michael"}).populate("patient")
-            .populate({path:"patient.weight" ,select:"weight"}).lean()
-        console.log(clinician)
+            await clinicianData.findOne({first_name: "Michael"}).populate("patient").lean()
+        
+        for(let i = 0; i < Object.keys(clinician.patient).length; i++){
+            const patientData = await patient.findById({_id:clinician.patient[i]._id}).populate('weight')
+            // console.log(patientData)
+            clinician.patient[i].weight = patientData.weight
+        }
+        console.log(clinician.patient[0])
         return res.render('allPatient.hbs', {data: clinician})
     }catch(err){
         return next(err)
