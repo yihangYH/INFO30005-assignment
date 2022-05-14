f456a7591810b6e5b4b416fbfccc42bcd28ec62f
DerexWang <jinchengw@student.unimelb.edu.au>
2022-05-14 15:28:48 +1000

    js

--- public/js/leaderBoard.js
+++ public/js/leaderBoard.js
@@ -1,3 +1,12 @@
+<<<<<<< Updated upstream
 function findMaxDataRecorded(bloodGlucose,) {
     console.log(data);
+=======
+function findMaxDataRecorded(bloodGlucose,weight,exercise,insulinTaken) {
+    console.log(bloodGlucose);
+    console.log(weight);
+    console.log(exercise);
+    console.log(insulinTaken);
+
+>>>>>>> Stashed changes
 }
