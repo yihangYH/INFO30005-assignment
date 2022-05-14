541a573efae6b7f5d185b8f5898725a4f17190c6
Yihang Liu <yihang1@student.unimelb.edu.au>
2022-05-14 13:38:35 +0800

    return name

--- public/js/leaderBoard.js
+++ public/js/leaderBoard.js
@@ -1,16 +1,21 @@
 function findMaxDataRecorded(bloodGlucose,weight,exercise,insulinTaken) {
     var count = 0;
+    var dataName="";
     if(bloodGlucose > count) {
         count = bloodGlucose;
+        dataName = "bloodGlucose";
     }
     if(weight > count) {
         count = weight;
+        dataName = "weight";
     }
     if(exercise > count) {
         count = exercise;
+        dataName = "exercise";
     }
     if(insulinTaken > count) {
         count = insulinTaken;
+        dataName = "insulinTaken";
     }
-    return count;
+    return dataName;
 }
