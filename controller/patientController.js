5e2451cc6594947a817940aa34d3b01512511491
Yihang Liu <yihang1@student.unimelb.edu.au>
2022-05-14 20:29:36 +0800

    Update patientController.js

--- controller/patientController.js
+++ controller/patientController.js
@@ -59,7 +59,6 @@ const updateData  = async(req,res,next) =>{
         const currentMonth = dateString.split('/')[1]
         const currentDay = dateString.split('/')[0]
         const patientData = await patient.findOne({_id:req.params.id}).populate("weight").populate("exercise").populate("bloodGlucose").populate("insulinTaken").lean();
-        console.log(patientData.bloodGlucose.length)
         //below we have logic set for updating data include blood glucose, body weight, insulin taken and exercise
         // need to check it data is not required or not empty first
         if(patientData.bloodGlucose.length == 0 && req.body.blood_glucose != "Not Required"){
@@ -68,8 +67,8 @@ const updateData  = async(req,res,next) =>{
                 time:dateString,
                 comment:req.body.blood_glucose_comment,
             })
-            await patient.findOneAndUpdate({_id:req.params.id}, {$push: {bloodGlucose: data._id}});
-            data.save() 
+            // await patient.findOneAndUpdate({_id:req.params.id}, {$push: {bloodGlucose: data._id}});
+            // data.save() 
         }else{
             if(req.body.blood_glucose != "" && req.body.blood_glucose != "Not Required"){
                 const leastTime = patientData.bloodGlucose[patientData.bloodGlucose.length - 1].time;
@@ -83,8 +82,8 @@ const updateData  = async(req,res,next) =>{
                 //update to data if the patient have not update today, 
                 // if patient already updated today, patient will not be able to update again
                 if(leastTime.split('/')[1] != currentMonth || leastTime.split('/')[0] != currentDay){
-                    await patient.findOneAndUpdate({_id:req.params.id}, {$push: {bloodGlucose: data._id}});
-                    data.save() 
+                    // await patient.findOneAndUpdate({_id:req.params.id}, {$push: {bloodGlucose: data._id}});
+                    // data.save() 
                 }
             }
         }
@@ -95,8 +94,8 @@ const updateData  = async(req,res,next) =>{
                 time:dateString,
                 comment:req.body.weight_comment,
             })
-            await patient.findOneAndUpdate({_id:req.params.id}, {$push: {weight: data._id}});
-            data.save() 
+            // await patient.findOneAndUpdate({_id:req.params.id}, {$push: {weight: data._id}});
+            // data.save() 
         }else{
             // same logic as above but for weight
             if(req.body.weight != "" && req.body.weight != "Not Required" ){
@@ -107,8 +106,8 @@ const updateData  = async(req,res,next) =>{
                     comment:req.body.weight_comment,
                 })
                 if(leastTime.split('/')[1] != currentMonth || leastTime.split('/')[0] != currentDay){
-                    await patient.findOneAndUpdate({_id:req.params.id}, {$push: {weight: data._id}});
-                    data.save() 
+                    // await patient.findOneAndUpdate({_id:req.params.id}, {$push: {weight: data._id}});
+                    // data.save() 
                 }
             }
         }
@@ -118,8 +117,8 @@ const updateData  = async(req,res,next) =>{
                 time:dateString,
                 comment:req.body.inssulin_comment,
             })
-            await patient.findOneAndUpdate({_id:req.params.id}, {$push: {insulinTaken: data._id}});
-            data.save()
+            // await patient.findOneAndUpdate({_id:req.params.id}, {$push: {insulinTaken: data._id}});
+            // data.save()
         }else{
             // same logic as above but for insulin taken
             if(req.body.insulin_taken != "" && req.body.insulin_taken != "Not Required" ){
@@ -130,8 +129,8 @@ const updateData  = async(req,res,next) =>{
                     comment:req.body.inssulin_comment,
                 })
                 if(leastTime.split('/')[1] != currentMonth || leastTime.split('/')[0] != currentDay){
-                    await patient.findOneAndUpdate({_id:req.params.id}, {$push: {insulinTaken: data._id}});
-                    data.save()
+                    // await patient.findOneAndUpdate({_id:req.params.id}, {$push: {insulinTaken: data._id}});
+                    // data.save()
                 }
             }
         }
@@ -141,8 +140,8 @@ const updateData  = async(req,res,next) =>{
                 time:dateString,
                 comment:req.body.exercise_comment,
             })
-            await patient.findOneAndUpdate({_id:req.params.id}, {$push: {exercise: data._id}});
-            data.save() 
+            // await patient.findOneAndUpdate({_id:req.params.id}, {$push: {exercise: data._id}});
+            // data.save() 
         }else{
             // same logic as above but for exercise
             if(req.body.exercise != "" && req.body.exercise != "Not Required" ){
@@ -153,8 +152,8 @@ const updateData  = async(req,res,next) =>{
                     comment:req.body.exercise_comment,
                 })
                 if(leastTime.split('/')[1] != currentMonth || leastTime.split('/')[0] != currentDay){
-                    await patient.findOneAndUpdate({_id:req.params.id}, {$push: {exercise: data._id}});
-                    data.save() 
+                    // await patient.findOneAndUpdate({_id:req.params.id}, {$push: {exercise: data._id}});
+                    // data.save() 
                 }
             }
         }
@@ -257,8 +256,9 @@ function caculateRate(patient){
     var rate  = findRate(maxCount);
     rate = (rate * 100)
     rate = Math.round(rate);
-    return [patient.first_name, rate,patient.userid]
+    return [patient.screen_name, rate,patient.userid]
 }
+
 
 function findRate(maxCount){
     var fisrtTime = maxCount[1][0].time;
