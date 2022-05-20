7fb3b9b560c8d14dd41b68f4345807cd3057a1a1
Laohan <hlaohan666@gmail.com>
2022-05-20 11:30:09 +0800

    Reformat login router

--- controller/loginController.js
+++ controller/loginController.js
@@ -1,6 +1,7 @@
 const {patient} = require('../models/patient.js')
 const {clinicianNote} = require("../models/data")
 const clinicianData = require("../models/clinician")
+const bcrypt = require('bcryptjs')
 
 const isAuthenticated = (req, res, next) => {
     // If user is not authenticated via passport, redirect to login page 
@@ -19,5 +20,78 @@ const isAuthenticated = (req, res, next) => {
     return next() 
 }
 
-module.exports = {isAuthenticated}
+const getEmpty = (req, res, next) => {
+    res.redirect('/login') 
+}
+
+const getChangePassword = (req, res, next) => {
+    res.render('changePassword')
+}
+
+const postChangePassword = (req, res, next) => {
+    console.log(req.body.oldPassword)
+    patient.findOne({ userid: req.body.userID },(err, user) => {
+      // Check if error connecting
+      if (err) {
+        // res.json({ success: false, message: err }); // Return error
+      } else {
+        user.verifyPassword(req.body.oldPassword, (err, valid) => { 
+            if (err) {
+                console.log('Incorrect password')
+                return
+            }
+            if (!valid) {
+                console.log('Incorrect password')
+                // res.send('Incorrect password')
+                return
+            }
+            bcrypt.hash(req.body.newPassword, 10, (err, hash) => { 
+                if (err) {
+                    return next(err) 
+                }
+                // Replace password with hash
+                patient.findOneAndUpdate({ userid: req.body.userID }, { password: hash }, (err, user) => {
+                    if (err) {
+                        // res.json({ success: false, message: err }); // Return error
+                    } else {
+                        // res.json({ success: true, message: 'Password has been changed!' }); // Return success
+                    }
+                });
+            })
+        })
+      }
+    })
+}
+
+const getLogin = (req, res, next) => {
+    res.render('login', { flash: req.flash('error'), title: 'Login' })
+}
+
+const logOut = (req, res, next) =>{
+    req.logOut();
+    delete req.user
+    req.session.destroy()
+    res.redirect('/');
+}
+
+const patientLogin = (req, res) => {
+    res.status("202")
+    res.statusMessage = req.user._id
+    res.send()
+}
+
+const clinicianLogin = (req, res) => {
+    res.status("202")
+    res.statusMessage = req.user._id
+    res.send()
+}
+
+module.exports = {isAuthenticated
+    ,getEmpty
+    ,getChangePassword
+    ,postChangePassword
+    ,getLogin
+    ,logOut
+    ,patientLogin
+    ,clinicianLogin}
 
--- routes/loginRouter.js
+++ routes/loginRouter.js
@@ -24,82 +24,31 @@ const clinicianController = require('../controller/clinicianController')
 //     }
 //     return next() 
 // }
-loginRouter.get('/', (req, res) => {
-    res.redirect('/login') 
-})
-loginRouter.get('/data/:id', loginController.isAuthenticated, patientController.getPatient)
+loginRouter.get('/', loginController.getEmpty)
+
+// loginRouter.get('/data/:id', loginController.isAuthenticated, patientController.getPatient)
 
 // loginRouter.get('/dashboard/:id', loginController.isAuthenticated, clinicianController.getClinician)
 
-loginRouter.get('/changePassword', (req, res) => {
-    res.render('changePassword')
-})
+loginRouter.get('/changePassword', loginController.getChangePassword)
 
-loginRouter.post('/changePassword', function(req, res) {
-    console.log(req.body.oldPassword)
-    patient.findOne({ userid: req.body.userID },(err, user) => {
-      // Check if error connecting
-      if (err) {
-        // res.json({ success: false, message: err }); // Return error
-      } else {
-        user.verifyPassword(req.body.oldPassword, (err, valid) => { 
-            if (err) {
-                console.log('Incorrect password')
-                return
-            }
-            if (!valid) {
-                console.log('Incorrect password')
-                // res.send('Incorrect password')
-                return
-            }
-            bcrypt.hash(req.body.newPassword, 10, (err, hash) => { 
-                if (err) {
-                    return next(err) 
-                }
-                // Replace password with hash
-                patient.findOneAndUpdate({ userid: req.body.userID }, { password: hash }, (err, user) => {
-                    if (err) {
-                        // res.json({ success: false, message: err }); // Return error
-                    } else {
-                        // res.json({ success: true, message: 'Password has been changed!' }); // Return success
-                    }
-                });
-            })
-        })
-      }
-    }); 
-})
+loginRouter.post('/changePassword', loginController.postChangePassword)
 
-loginRouter.get('/login', (req, res) => {
-    res.render('login', { flash: req.flash('error'), title: 'Login' })
-})
+loginRouter.get('/login', loginController.getLogin)
 
-loginRouter.post('/logout', function(req, res){
-    req.logOut();
-    delete req.user
-    req.session.destroy()
-    res.redirect('/');
-});
+loginRouter.post('/logout', loginController.logOut)
 
 loginRouter.post('/patientlogin', 
     passport.authenticate('local', {
         failureRedirect: '/login', failureFlash: true 
     }),
-    function(req, res) {
-        res.status("202")
-        res.statusMessage = req.user._id
-        res.send()
-    });
+    loginController.patientLogin);
 
 loginRouter.post('/doctorLogin', 
     passport.authenticate('client-login', {
         failureRedirect: '/login', failureFlash: true 
     }),
-    function(req, res) {
-        res.status("202")
-        res.statusMessage = req.user._id
-        res.send()
-    });
+    loginController.clinicianLogin);
 
 
 // loginRouter.post('/data/:id', patientController.updateData)
