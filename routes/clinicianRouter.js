52845d5d5eb6edf5e23599c2521a62b3c963ab9c
liuka <liuka@student.unimelb.edu.au>
2022-05-21 21:53:59 +0800

    formatted routers/*js code

--- routes/clinicianRouter.js
+++ routes/clinicianRouter.js
@@ -3,26 +3,34 @@ const clinicianRouter = express.Router()
 const loginController = require('../controller/loginController')
 const clinicianController = require('../controller/clinicianController')
 
-clinicianRouter.get('/dashboard/:id', clinicianController.isAuthenticated, clinicianController.getClinician)
+clinicianRouter.get('/dashboard/:id', clinicianController.isAuthenticated, 
+                    clinicianController.getClinician)
 
-clinicianRouter.get('/:id/newPatient', clinicianController.isAuthenticated, clinicianController.createPatient)
+clinicianRouter.get('/:id/newPatient', clinicianController.isAuthenticated, 
+                    clinicianController.createPatient)
 
 clinicianRouter.post('/:id/newPatient', clinicianController.CreatePatient)
 
-clinicianRouter.get('/:id/:patientID/updatePatient', clinicianController.isAuthenticated, clinicianController.getUpdatePatient)
+clinicianRouter.get('/:id/:patientID/updatePatient', clinicianController.isAuthenticated, 
+                    clinicianController.getUpdatePatient)
 
-clinicianRouter.post('/:id/:patientID/updatePatient',  clinicianController.updatePatient)
+clinicianRouter.post('/:id/:patientID/updatePatient', clinicianController.updatePatient)
 
-clinicianRouter.get('/:id/comment', clinicianController.isAuthenticated, clinicianController.comment)
+clinicianRouter.get('/:id/comment', clinicianController.isAuthenticated, 
+                    clinicianController.comment)
 
-clinicianRouter.get('/:id/:patientID/patientDetail', clinicianController.isAuthenticated, clinicianController.getPatientDetail)
+clinicianRouter.get('/:id/:patientID/patientDetail', clinicianController.isAuthenticated, 
+                    clinicianController.getPatientDetail)
 
-clinicianRouter.post('/:id/:patientID/updateMessage' ,clinicianController.updateSupportMessage)
+clinicianRouter.post('/:id/:patientID/updateMessage', 
+                    clinicianController.updateSupportMessage)
 
-clinicianRouter.post('/:id/:patientID/updateNote' ,clinicianController.updateNote)
+clinicianRouter.post('/:id/:patientID/updateNote', 
+                    clinicianController.updateNote)
 
 // to be remove, it used to create clinician
 clinicianRouter.get('/creatClincianTemp', clinicianController.getTemp)
+
 // to be remove, it used to create clinician
 clinicianRouter.post('/creatClincianTemp', clinicianController.createTemp)
 
--- routes/patientRouter.js
+++ routes/patientRouter.js
@@ -2,25 +2,31 @@ const express = require('express')
 
 const patientRouter = express.Router()
 
-
 const patientController = require('../controller/patientController')
 
+patientRouter.get('/data/:id', patientController.isAuthenticated,
+                    patientController.getPatient)
 
-patientRouter.get('/data/:id', patientController.isAuthenticated, patientController.getPatient)
+patientRouter.get('/data/:id/bloodGlucose', patientController.isAuthenticated, 
+                    patientController.getPassBloodGlucose)
 
-patientRouter.get('/data/:id/bloodGlucose', patientController.isAuthenticated,  patientController.getPassBloodGlucose)
+patientRouter.get('/data/:id/weight', patientController.isAuthenticated, 
+                    patientController.getPassWeight)
 
-patientRouter.get('/data/:id/weight', patientController.isAuthenticated, patientController.getPassWeight)
+patientRouter.get('/data/:id/insulinTaken', patientController.isAuthenticated, 
+                    patientController.getPassInsulin)
 
-patientRouter.get('/data/:id/insulinTaken', patientController.isAuthenticated, patientController.getPassInsulin)
+patientRouter.get('/data/:id/exercise', patientController.isAuthenticated, 
+                    patientController.getPassExercise)
 
-patientRouter.get('/data/:id/exercise', patientController.isAuthenticated, patientController.getPassExercise)
+patientRouter.get('/data/:id/leaderboard', patientController.isAuthenticated, 
+                    patientController.getLeaderboard)
 
-patientRouter.get('/data/:id/leaderboard', patientController.isAuthenticated, patientController.getLeaderboard)
+patientRouter.post('/checkPatient', patientController.isAuthenticated, 
+                    patientController.checkPatient)
 
-patientRouter.post('/checkPatient', patientController.isAuthenticated, patientController.checkPatient)
-
-patientRouter.post('/data/:id', patientController.isAuthenticated, patientController.updateData)
+patientRouter.post('/data/:id', patientController.isAuthenticated, 
+                    patientController.updateData)
 
 patientRouter.post('/data/:id/updateBio',patientController.updateBioMessage)
 
